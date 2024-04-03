261a36b22e86a1fa6771e6083fe83887ba46ef48
Sébastien <seb.juillet987@hotmail.fr>
2024-04-03 16:34:32 +0200

    minor correction in collapse component

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -18,7 +18,7 @@ import "./index.scss"
 
 export default function Collapse({title, text}) {
     //Declares a state variable and a function to update the state variable. Defaults to false.
-    const [invisible, setInvisible] = useState(false)
+    const [visible, setVisible] = useState(false)
     
    
     return (
@@ -29,12 +29,12 @@ export default function Collapse({title, text}) {
                 </h3>
                 <img 
                 // Allows you to change the status of the variable on click
-                onClick={() => setInvisible(!invisible)}
-                className={`${invisible && "close-collapse"} arrow-collapse`}
+                onClick={() => setVisible(!visible)}
+                className={`${visible && "close-collapse"} arrow-collapse`}
                 src={arrow} 
                 alt="flêche déploiement menu" />
             </div>    
-            <span className={invisible ? "content-collapse text-visible" : "content-collapse text-invisible"}>
+            <span className={visible ? "content-collapse text-visible" : "content-collapse text-invisible"}>
                 <div>{text}</div>
             </span>
         </>
